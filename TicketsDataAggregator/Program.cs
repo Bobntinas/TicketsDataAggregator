f7f89c1303b780b35d5ee9d374eadd5797ae419b
Bob <bob.ntinas@hotmail.com>
2024-03-07 20:23:08 +0200

    Add project files.

---
+++ TicketsDataAggregator/Program.cs
@@ -0,0 +1,22 @@
+﻿using TicketsDataAggregator.FileAccess;
+using TicketsDataAggregator.TicketsAggregation;
+
+const string TicketsFolder = @"C:\file-path";
+
+try
+{
+    var ticketsAggregator = new TicketsAggregator(
+        TicketsFolder,
+        new FileWriter(),
+        new DocumentsFromPdfsReader());
+
+    ticketsAggregator.Run();
+}
+catch(Exception ex)
+{
+    Console.WriteLine("An exception occured. " +
+        "Exception message: " + ex.Message);
+}
+
+Console.WriteLine("Press any key to close.");
+Console.ReadKey();
